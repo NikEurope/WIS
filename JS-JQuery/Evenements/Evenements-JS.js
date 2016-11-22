fdf5d7b7f6ea8491c6d95c1fde8f30b760c1df36
YOUR NAME <nik.europe@gmail.com>
2016-11-22 14:12:31 +0100

    5

---
+++ JS-JQuery/Evenements/Evenements-JS.js
@@ -0,0 +1,44 @@
+
+
+// Les événements JS
+
+// 1 Les événements JS
+
+
+
+
+
+// 2 L'héritage des propriétés et des méthodes
+
+
+
+
+
+
+
+// 3 Les attributs  Via l'objet Element
+
+
+
+
+// 4  La classe
+
+
+
+
+// 5  Récupérer du HTML 
+
+
+// 6 innerText et textContent
+
+
+
+
+// 7 Lister et parcourir des noeuds enfants
+
+
+
+
+// 8 Création de l'élément
+
+ 
